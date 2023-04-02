1b960189f5ddab82a7ed88327f8a63670c67910e
GoldFrosch <goldfrosch@daum.net>
2023-04-02 19:30:01 +0900

    feat. monorepo setting

--- qc-client/apps/app/vite.config.ts
+++ qc-client/apps/app/vite.config.ts
@@ -1,6 +1,7 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 import viteTsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
@@ -21,16 +22,8 @@ export default defineConfig({
     viteTsConfigPaths({
       root: "../../",
     }),
+    vanillaExtractPlugin()
   ],
-
-  // Uncomment this if you are using workers.
-  // worker: {
-  //  plugins: [
-  //    viteTsConfigPaths({
-  //      root: '../../',
-  //    }),
-  //  ],
-  // },
 
   test: {
     globals: true,
