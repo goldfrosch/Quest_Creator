067c155a342771bfca994cdfb45022a9737c86d7
GoldFrosch <goldfrosch@daum.net>
2023-05-20 20:43:40 +0900

    refactor(common): html element css type to all optional (Partial)

--- qc-client/libs/qc-common/src/lib/types/dom-style.ts
+++ qc-client/libs/qc-common/src/lib/types/dom-style.ts
@@ -1,13 +1,15 @@
-export type HTMLElementStyle = CSSStyleDeclaration;
-
-export type ColorTheme = Pick<HTMLElementStyle, 'backgroundColor' | 'color'>;
+export type HTMLElementStyle = Partial<CSSStyleDeclaration>;
 
 export type BorderTheme = Pick<
   HTMLElementStyle,
   'borderColor' | 'borderRadius' | 'borderWidth'
 >;
 
+export type ColorTheme = Pick<HTMLElementStyle, 'backgroundColor' | 'color'>;
+
 export type FlexPatternTheme = Pick<
   HTMLElementStyle,
   'alignItems' | 'justifyContent' | 'flexDirection' | 'gap'
 >;
+
+export type FontTheme = Pick<HTMLElementStyle, 'fontSize' | 'fontWeight'>;
